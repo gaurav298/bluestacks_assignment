027c4db8bc3debe379cdf75ff5902fb0a36821b2
mmt7538 <gaurav.mehta@go-mmt.com>
2019-05-04 12:57:41 +0530

    Updates

---
+++ precache-manifest.0bd8129c9eb56964139ac2ee1f954d55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42835e991f2fddc8328c407a61e3d312",
+    "url": "/bluestacks_assignment/index.html"
+  },
+  {
+    "revision": "d3231de898a9bfc1d95c",
+    "url": "/bluestacks_assignment/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "ea48714562bf84f6f190",
+    "url": "/bluestacks_assignment/static/css/main.1266ad10.chunk.css"
+  },
+  {
+    "revision": "d3231de898a9bfc1d95c",
+    "url": "/bluestacks_assignment/static/js/2.950bf9dd.chunk.js"
+  },
+  {
+    "revision": "ea48714562bf84f6f190",
+    "url": "/bluestacks_assignment/static/js/main.840fb427.chunk.js"
+  },
+  {
+    "revision": "a608289df50effa0f5ad",
+    "url": "/bluestacks_assignment/static/js/runtime~main.0179f068.js"
+  }
+]);
